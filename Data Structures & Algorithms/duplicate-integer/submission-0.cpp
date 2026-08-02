50105a0ad25e4c70c37a32e21b69f830bebf5d7f
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2026-08-02 09:29:02 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        int size= nums.size();
+        for(int i=0; i<size;i++){
+            for(int j=i+1; j<size; j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }        
+        return false;
+    }
+};
---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+
+        /*
+        int size= nums.size();
+        for(int i=0; i<size;i++){
+            for(int j=i+1; j<size; j++){
+                if(nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }        
+        return false;
+        */
+        if (nums.empty()) return false;
+        std::sort(nums.begin(), nums.end());
+        for(size_t i=0; i < nums.size() - 1; i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+};
