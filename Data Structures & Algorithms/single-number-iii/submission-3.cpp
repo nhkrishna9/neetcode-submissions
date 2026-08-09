b567f2389f95daafa7d433e9ac80cc5f606cc70c
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2026-08-09 18:35:25 +0530

    Add: single-number-iii - submission-3

---
+++ Data Structures & Algorithms/single-number-iii/submission-3.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+
+        int xorOfAllNums=0;
+        for(int num:nums){
+            xorOfAllNums ^= num;
+        }
+        int lastsetbitOfxorOfAllNums = xorOfAllNums & ~(xorOfAllNums-1);
+
+        vector<int> lastsetbitOfxorOfAllNumsIsSet;
+        vector<int> lastsetbitOfxorOfAllNumsIsNotSet;
+
+        for(int num:nums){
+            if(num & lastsetbitOfxorOfAllNums){
+                lastsetbitOfxorOfAllNumsIsSet.push_back(num);
+            }
+            else{
+                lastsetbitOfxorOfAllNumsIsNotSet.push_back(num);
+            }
+        }
+
+        vector<int> ans;
+        int tmp=0;
+        for(int num:lastsetbitOfxorOfAllNumsIsSet){
+            tmp^=num;
+        }
+        ans.push_back(tmp);
+        tmp=0; // resetting tmp
+        for(int num:lastsetbitOfxorOfAllNumsIsNotSet){
+            tmp^=num;
+        }
+        ans.push_back(tmp);
+
+        return ans;
+        
+    }
+};
