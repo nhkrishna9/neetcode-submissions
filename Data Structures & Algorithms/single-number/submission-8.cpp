40e43fe97d0ec3ad6e177da694965b97ee6d74da
Hari Krishna Nalla <149000167+nhkrishna9@users.noreply.github.com>
2026-08-09 16:17:27 +0530

    Add: single-number - submission-8

---
+++ Data Structures & Algorithms/single-number/submission-8.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        /*
+        int ans = nums[0];
+        for(int i=1;i<nums.size(); i++){
+            ans = ans^nums[i];
+        }
+        return ans;
+        */
+        /*
+        for(int i=0;i<nums.size();i++){
+            bool foundOnce = true;
+            for(int j=0; j<nums.size();j++){
+                if(i!=j && nums[i] == nums[j]){
+                    foundOnce = false;
+                    break;
+                }
+            }
+            if(foundOnce){
+                return nums[i];
+            }
+
+        }
+        return -1;
+        */
+        unordered_set<int> hash;
+        for(int num:nums){
+            if(hash.find(num)==hash.end()){
+                hash.insert(num);
+            }
+            else{
+                hash.erase(num);
+            }
+        }
+        return *hash.begin();
+    }
+};
